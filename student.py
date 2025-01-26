4b152d7fcd61c4661ee5bc4441d671e889ee21b9
SriSantoshibalaji <srisantoshi.b@gmail.com>
2025-01-26 20:19:00 +0530

    Add files via upload
    1.multiple conditions are given so elif statement used.
    2. used operator such<,+<,>.
    3. used conditional statement if,elif,else.

---
+++ student.py
@@ -0,0 +1,13 @@
+num=int(input("enter a number:"))
+if (num>=90):
+   print("grade A+")
+elif(num>=80 and num<89):
+   print("grade A")
+elif(num>=70 and num<79):
+   print("grade B+")
+elif(num>=60 and num<69):
+   print("grade B")
+elif(num>=50 and num<59):
+   print("grade C")
+else:
+   print("fail")
