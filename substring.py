4c6a97aa34db117ac32d01dfcb3b2fc4816597f1
SriSantoshibalaji <srisantoshi.b@gmail.com>
2025-01-28 21:26:48 +0530

    Add files via upload
    1.sliced the substring from string.
    2.using slicing operator

---
+++ substring.py
@@ -0,0 +1,9 @@
+str="welcome to python programing"
+substring_1=str[0:7] 
+print(substring_1)
+substring_2=str[8:10]
+print(substring_2)
+substring_3=str[12:15]
+print(substring_3)
+substring_4=str[17:27]
+print(substring_4)
