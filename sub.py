2ed695a2aaf547a347440a313f34be2ec045cf39
SriSantoshibalaji <srisantoshi.b@gmail.com>
2025-01-31 07:43:48 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,12 @@
+num1=int(input("enter a number:"))
+num2=int(input("enter a number"))
+def add_and_subtract(num1,num2):
+    addition=num1+num2
+if (num1>num2):
+    substraction=num1-num2
+else:
+    substraction=num2-num1
+
+
+print("addition result",addition)
+print("subtraction result",substraction)
