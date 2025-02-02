f610f4ba96eb14400d19ca167357547b358df635
SriSantoshibalaji <srisantoshi.b@gmail.com>
2025-02-02 08:51:38 +0530

    Add files via upload
    1.itialized  two variables .
    2.defined add or subtract.
    3.using if else addition subtraction operation are performed.

--- sub.py
+++ sub.py
@@ -1,12 +1,13 @@
-num1=int(input("enter a number:"))
-num2=int(input("enter a number"))
-def add_and_subtract(num1,num2):
-    addition=num1+num2
-if (num1>num2):
-    substraction=num1-num2
-else:
-    substraction=num2-num1
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+def add_and_subtract(a, b):
+    addition = a + b
+    if a > b:
+        subtraction = a - b
+    else:
+        subtraction = b - a
+    
+sum_result, sub_result = add_and_subtract(a,b)
 
-
-print("addition result",addition)
-print("subtraction result",substraction)
+print("Addition result:", sum_result)
+print("Subtraction result:", sub_result)
